a2b4a9eec432606df7825ee6507f3aa20560f901
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-08-11 04:44:46 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+window.EMPIRE_CONFIG = {
+  API_BASE: "https://empireforge-pwa-v35-2.onrender.com"
+};
---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='ef-v27-8-stable-chat-war';
+const ASSETS=['./','./index.html','./manifest.json','./assets/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.mode==='navigate'||new URL(req.url).pathname.endsWith('/index.html')){
+    e.respondWith(fetch(req).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(req,copy));return r}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html'))));
+    return;
+  }
+  e.respondWith(caches.match(req).then(r=>r||fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(req,copy));return resp})));
+});
