920a3c46ad004157dda5d736c44c2150dc9a9508
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-08-13 15:13:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='empire-forge-v37-21';
+const CACHE='empire-forge-v37-22';
 const ASSETS=['./','./index.html','./config.js','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()]));});
